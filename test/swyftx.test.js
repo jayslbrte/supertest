255cef756803a8be03f42622c0f0f0086d3eb228
jayson <jays.labarrete@gmail.com>
2021-10-05 19:25:01 +1100

    updated test.

--- test/swyftx.test.js
+++ test/swyftx.test.js
@@ -1,40 +1,45 @@
-import supertest from 'supertest';
-import {expect} from 'chai';
-import { interfaces, it } from 'mocha';
-const request = supertest('https://private-anon-87a2e80e45-swyftx.apiary-mock.com/');
-
-let token
+import supertest from "supertest";
+import { expect } from "chai";
+import { interfaces, it } from "mocha";
+const request = supertest(
+  "https://private-anon-87a2e80e45-swyftx.apiary-mock.com/"
+);
+const key = "7r4hTa2Yb...";
+let token;
 
 describe("Swyftx QE Test", function (done) {
-   it("Retrieves a new access token", function(){
-       return request.post('auth/refresh/')
-       .set('Content-Type','application/json')
-       .send({apiKey: "7r4hTa2Yb..."})
-       .then((res) => {
-        expect(res.body).to.not.be.empty;
+  it("Retrieves a new access token", function () {
+    return request
+      .post("auth/refresh/")
+      .set("Content-Type", "application/json")
+      .send({ apiKey: key })
+      .then((res) => {
+        expect(res.body).
         token = res.body.accessToken;
-       });      
-    });
+      });
+  });
 
-   it("Get Profile", function(){
-       return request.get('user/')
-       .set('Content-Type','application/json')
-       .set(`Authorization`, `Bearer ${token}`)
-       .then((res) => {
+  it("Get Profile", function () {
+    return request
+      .get("user/")
+      .set("Content-Type", "application/json")
+      .set(`Authorization`, `Bearer ${token}`)
+      .then((res) => {
         expect(res.body).to.not.be.empty;
-        expect(res.body.profile.email).to.eq('john.doe@example.com');
-        expect(res.body.profile.name.first).to.eq('John');
-        expect(res.body.profile.name.last).to.eq('Doe');
-       });
-    });
+        expect(res.body.profile.email).to.eq("john.doe@example.com");
+        expect(res.body.profile.name.first).to.eq("John");
+        expect(res.body.profile.name.last).to.eq("Doe");
+      });
+  });
 
-    it("Get Balance", function(){
-        return request.get('user/balance/')
-        .set('Content-Type','application/json')
-        .set(`Authorization`,`Bearer ${token}`)
-        .then((res) => {
-          expect(res.body)
-        });
-    });   
-
+  it("Get Balance", function () {
+    return request
+      .get("user/balance/")
+      .set("Content-Type", "application/json")
+      .set(`Authorization`, `Bearer ${token}`)
+      .then((res) => {
+        expect(res.body);
+        console.log(res.body);
+      });
+  });
 });
